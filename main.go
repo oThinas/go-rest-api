c3e12c12b58aa296362be6a84cd8eea766afb82c
oThinas <thiagomartinsprado@gmail.com>
2024-09-19 18:08:07 -0300

    feat: add cors

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"go-rest-api/database"
 	"go-rest-api/routes"
 	"net/http"
+
+	"github.com/gorilla/handlers"
 )
 
 func main() {
 	database.Connect()
 	r := routes.HandleRequest()
-	http.ListenAndServe(":8080", r)
+	http.ListenAndServe(":8080", handlers.CORS(handlers.AllowedOrigins([]string{"http://localhost:5173"}))(r))
 }
